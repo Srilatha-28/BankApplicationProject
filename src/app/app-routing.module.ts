d91b554697519fdafabebc82581406fc50989e5d
Srilatha-28 <srilathareddy2805@gmail.com>
2021-01-24 18:45:28 +0530

    files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountComponent } from './account/account.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { CustomeraccountDetailsComponent } from './customeraccount-details/customeraccount-details.component';
+import { DepositComponent } from './deposit/deposit.component';
+import { TransferComponent } from './transfer/transfer.component';
+import { UpdateAccountComponent } from './update-account/update-account.component';
+import { WithdrawComponent } from './withdraw/withdraw.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'accounts', component: AccountComponent},
+  {path:'create-account', component: CreateAccountComponent},
+  {path:'update-account/:accountId', component: UpdateAccountComponent},
+  {path:'customeraccount-details/:accountId', component: CustomeraccountDetailsComponent},
+  {path:'transfer', component: TransferComponent},
+  {path:'deposit', component: DepositComponent},
+  {path:'withdraw', component: WithdrawComponent},
+  {path:'', redirectTo:'accounts', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { UpdateAccountComponent } from './update-account/update-account.component';
+import { FormsModule } from '@angular/forms';
+import { CustomeraccountDetailsComponent } from './customeraccount-details/customeraccount-details.component';
+import { DepositComponent } from './deposit/deposit.component';
+import { WithdrawComponent } from './withdraw/withdraw.component';
+import { TransferComponent } from './transfer/transfer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent,
+    CreateAccountComponent,
+    UpdateAccountComponent,
+    CustomeraccountDetailsComponent,
+    DepositComponent,
+    WithdrawComponent,
+    TransferComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
